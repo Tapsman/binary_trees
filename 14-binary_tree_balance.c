aef28f78549d19fa6d757c9e9e84d2a53b269467
Tapsman <tapiwanyoni140@gmail.com>
2024-06-05 08:48:21 -0700

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,8 +19,8 @@ size_t binary_tree_height_c(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			left = tree->left ? 1 + binary_tree_height_c(tree->left) : 0;
-			right = tree->right ? 1 + binary_tree_height_c(tree->right) : 0;
+			left = tree->left ? 1 + binary_tree_height_c(tree->left) : 1;
+			right = tree->right ? 1 + binary_tree_height_c(tree->right) : 1;
 		}
 		return ((left > right) ? left : right);
 	}
