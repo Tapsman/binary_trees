a59fc7e657959c73a74fc2860dd8cb9a7fbba0ca
Tapsman <tapiwanyoni140@gmail.com>
2024-06-05 09:15:11 -0700

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -30,7 +30,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 			}
 			return (1);
 		}
-		else if (!tree->left && !tree->right)
+		else if (!tree->left)
+		{
+			return (1);
+		}
+		else if (!tree->right)
 		{
 			return (1);
 		}
