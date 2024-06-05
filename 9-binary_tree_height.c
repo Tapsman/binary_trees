67e3efb87978c24ccfeb38f0019d5c06dbbd5cf6
Tapsman <tapiwanyoni140@gmail.com>
2024-06-05 07:50:42 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -22,6 +22,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 			right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
-		return ((left > right) ? left : r);
+		return ((left > right) ? left : right);
 	}
 }
